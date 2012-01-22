a08fffdc78a79435e1265302048f7fe3c2e60a8e
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-01-22 15:30:20 -0500

    Initial commit to oauthable.

---
+++ test/test_oauthable.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestOauthable < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
